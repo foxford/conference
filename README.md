0b37bc400c687c4c3a8f227c3c5f3ade40388fa9
Andrei Nesterov <ae.nesterov@gmail.com>
2018-12-05 10:19:30 -0800

    Add support of Travis CI

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Conferences
 
+[![Build Status][travis-img]][travis]
+
 A WebRTC based conference media server.
 
 
@@ -9,3 +11,5 @@ A WebRTC based conference media server.
 The source code is provided under the terms of [the MIT license][license].
 
 [license]:http://www.opensource.org/licenses/MIT
+[travis]:https://travis-ci.com/netology-group/conferences?branch=master
+[travis-img]:https://travis-ci.com/netology-group/conferences.png?branch=master
---
+++ deploy/docs.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [[ ! ${SFTP_USER} ]]; then echo "SFTP_USER isn't specified" 1>&2; exit 1; fi
+if [[ ! ${SFTP_PASSWORD} ]]; then echo "SFTP_PASSWORD isn't specified" 1>&2; exit 1; fi
+
+PROJECT=${PROJECT:-'conferences'}
+BUCKET=${BUCKET:-'docs-netology-group.services'}
+HOST=${HOST:-'sftp.selcdn.ru'}
+SOURCE=${SOURCE:-'docs/book'}
+
+lftp -c "open -u ${SFTP_USER},${SFTP_PASSWORD} ${HOST}; mirror --parallel=4 --no-empty-dirs --no-perms --exclude-glob .DS_Store --reverse --verbose -e ${SOURCE} ${BUCKET}/${PROJECT}"
