2093bc95378c8ca0c288cc759480b8abbeac21ce
Andrei Nesterov <ae.nesterov@gmail.com>
2018-12-15 18:49:29 -0800

    Add support for serialization and deserialization for transport::compat::Envelope

--- src/transport/compat.rs
+++ src/transport/compat.rs
@@ -1,14 +1,42 @@
+use super::MessageProperties;
+use failure::Error;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub(crate) struct Envelope {
-    pub(crate) payload: String,
+    payload: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    properties: Option<MessageProperties>,
 }
 
 impl Envelope {
-    pub(crate) fn new(payload: &str) -> Self {
+    pub(crate) fn new(payload: &str, properties: Option<MessageProperties>) -> Self {
         Self {
             payload: payload.to_owned(),
+            properties: properties,
         }
     }
+}
+
+pub(crate) fn to_envelope<T>(
+    payload: &T,
+    properties: Option<MessageProperties>,
+) -> Result<Envelope, Error>
+where
+    T: serde::Serialize,
+{
+    let payload_s = serde_json::to_string(payload)?;
+    let envelope = Envelope::new(&payload_s, properties);
+    Ok(envelope)
+}
+
+pub(crate) fn from_envelope<T>(envelope: Envelope) -> Result<(T, MessageProperties), Error>
+where
+    T: serde::de::DeserializeOwned,
+{
+    let payload = serde_json::from_str::<T>(&envelope.payload)?;
+    let properties = envelope
+        .properties
+        .expect("An incoming message envelope must contain properties.");
+    Ok((payload, properties))
 }
