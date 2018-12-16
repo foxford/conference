fdaf9e391af92453b9ee538a9e1315e842c77873
Andrei Nesterov <ae.nesterov@gmail.com>
2018-12-16 09:24:25 -0800

    Add app::Agent and app::AgentBuilder types

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -2,105 +2,165 @@ use crate::transport;
 use failure::{err_msg, Error};
 use log::{error, info, warn};
 use rumqtt::{MqttClient, MqttOptions, QoS};
+use uuid::Uuid;
 
 mod config;
+mod janus;
+
+#[derive(Debug)]
+pub(crate) struct AgentBuilder {
+    label: String,
+    application: transport::ApplicationIdentity,
+    backend: transport::ApplicationName,
+}
+
+impl AgentBuilder {
+    fn new(
+        label: &str,
+        application: transport::ApplicationIdentity,
+        backend: transport::ApplicationName,
+    ) -> Self {
+        Self {
+            label: label.to_owned(),
+            application,
+            backend,
+        }
+    }
+
+    fn start(
+        self,
+        config: &config::Mqtt,
+    ) -> Result<(Agent, crossbeam_channel::Receiver<rumqtt::Notification>), Error> {
+        let client_id = Self::mqtt_client_id(&self.application.agent_id(&self.label));
+        let options = Self::mqtt_options(&client_id, &config)?;
+        let (tx, rx) = MqttClient::start(options)?;
+
+        let mut agent = Agent::new(self.application, self.backend, tx);
+        let group = agent.application.group("loadbalancer");
+        agent.tx.subscribe(
+            agent.backend_responses_subscription(&group),
+            QoS::AtLeastOnce,
+        )?;
+
+        Ok((agent, rx))
+    }
+
+    fn mqtt_client_id(agent_id: &transport::AgentId) -> String {
+        format!("v1.mqtt3/agents/{agent_id}", agent_id = agent_id)
+    }
+
+    fn mqtt_options(client_id: &str, config: &config::Mqtt) -> Result<MqttOptions, Error> {
+        let uri = config.uri.parse::<http::Uri>()?;
+        let host = uri.host().ok_or_else(|| err_msg("missing MQTT host"))?;
+        let port = uri
+            .port_part()
+            .ok_or_else(|| err_msg("missing MQTT port"))?;
+
+        Ok(MqttOptions::new(client_id, host, port.as_u16()).set_keep_alive(30))
+    }
+}
+
+pub(crate) struct Agent {
+    application: transport::ApplicationIdentity,
+    backend: transport::ApplicationName,
+    tx: rumqtt::MqttClient,
+}
+
+impl Agent {
+    fn new(
+        application: transport::ApplicationIdentity,
+        backend: transport::ApplicationName,
+        tx: MqttClient,
+    ) -> Self {
+        Self {
+            application,
+            backend,
+            tx,
+        }
+    }
+
+    fn publish<T>(&mut self, topic: &str, payload: &T) -> Result<(), Error>
+    where
+        T: serde::Serialize,
+    {
+        use crate::transport::compat::to_envelope;
+
+        let message = to_envelope(payload, None)?;
+        let bytes = serde_json::to_string(&message)?;
+
+        self.tx
+            .publish(topic, QoS::AtLeastOnce, bytes)
+            .map_err(|_| err_msg(format!("Failed to publish an MQTT message: {:?}", message)))
+    }
+
+    fn backend_input_topic(&self, backend_agent_id: &transport::AgentId) -> String {
+        format!(
+            "agents/{backend_agent_id}/api/v1/in/{app_name}",
+            backend_agent_id = backend_agent_id,
+            app_name = self.application.name()
+        )
+    }
+
+    fn backend_responses_subscription(&self, group: &transport::ApplicationGroup) -> String {
+        format!(
+            "$share/{group}/apps/{backend_name}/api/v1/responses",
+            group = group,
+            backend_name = &self.backend,
+        )
+    }
+}
 
 pub(crate) fn run() {
     // Config
     let config = config::load().expect("Failed to load config");
     info!("App config: {:?}", config);
 
-    // Ids
-    let agent_label = "a";
-    let agent_id = transport::AgentId::new(
-        &agent_label,
-        &config.identity.account_id,
-        &config.identity.audience,
-    );
-    let name = transport::ApplicationName::new(&config.identity.label, &config.identity.audience);
-    let group = transport::ApplicationGroup::new("loadbalancer", name.clone());
-    let backend_name = transport::ApplicationName::new(&config.backend.label, &config.backend.audience);
-
-    // MQTT client
-    let mqtt_client_id = format!("v1.mqtt3/agents/{agent_id}", agent_id = agent_id);
-    let mqtt_options =
-        mqtt_options(&mqtt_client_id, &config.mqtt).expect("Failed to initialize a MQTT client");
-    let (mut mqtt_tx, mqtt_rx) =
-        MqttClient::start(mqtt_options).expect("Failed to start a MQTT client");
-
-    // MQTT subscriptions
-    let sub_responses_backend = format!(
-        "$share/{group}/apps/{backend_name}/api/v1/responses",
-        group = group,
-        backend_name = backend_name
-    );
-//    let sub_events_backend = format!(
-//        "$share/{group}/apps/{backend_name}/api/v1/events/+",
-//        group = group,
-//        backend_name = config.backend.name
-//    );
-//    let sub_out_others = format!(
-//        "$share/{group}/agents/+/api/v1/out/{name}",
-//        group = group,
-//        name = name
-//    );
-    mqtt_tx
-        .subscribe(sub_responses_backend, QoS::AtLeastOnce)
-        .expect("Failed to subscribe to backend responses topic");
-//    mqtt_tx
-//        .subscribe(sub_events_backend, QoS::AtLeastOnce)
-//        .expect("Failed to subscribe to backend events topic");
-//    mqtt_tx
-//        .subscribe(sub_out_others, QoS::AtLeastOnce)
-//        .expect("Failed to subscribe to incoming messages topic");
-
-    use crate::backend::janus;
-    use crate::transport::compat::Envelope;
-
-    let message = {
-        let transaction = janus::Transaction::new("foobar", agent_id);
-
-        let payload =
-            serde_json::to_string(&janus::SessionCreateRequest::new(transaction)).unwrap();
-        let envelope = Envelope::new(&payload);
-        serde_json::to_string(&envelope).unwrap()
-    };
-    info!("req: {:?}", message);
+    // Agent
+    let (mut tx, rx) = AgentBuilder::new("a", config.identity.clone(), config.backend.clone())
+        .start(&config.mqtt)
+        .expect("Failed to create an agent");
 
     // TODO: derive a backend agent id from a status message
-    let pub_in_backend = format!(
-        "agents/{backend_agent_id}/api/v1/in/{name}",
-        backend_agent_id = "a.00000000-0000-1071-a000-000000000000.example.org",
-        name = name
+    let backend_agent_id = transport::AgentId::new(
+        "a",
+        Uuid::parse_str("00000000-0000-1071-a000-000000000000").expect("Failed to parse UUID"),
+        "example.org",
     );
-    mqtt_tx
-        .publish(pub_in_backend, QoS::AtLeastOnce, message)
-        .unwrap();
 
-    for message in mqtt_rx {
+    // TODO: Replace with Real-Time Connection data
+    let room_id = Uuid::new_v4();
+    let rtc_id = Uuid::new_v4();
+
+    // Creating a Janus Gateway session
+    let req = janus::create_session_request(room_id, rtc_id).expect("Failed to build a request");
+    tx.publish(&tx.backend_input_topic(&backend_agent_id), &req)
+        .expect("Failed to publish a message");
+
+    for message in rx {
         match message {
-            rumqtt::client::Notification::Publish(message) => {
-                match serde_json::from_slice::<Envelope>(message.payload.as_slice()) {
-                    Ok(envelope) => {
-                        match serde_json::from_str::<janus::Response>(&envelope.payload) {
-                            Ok(resp) => info!("{:?}", resp),
-                            _ => error!("bad janus message format = {:?}", envelope.payload),
-                        }
+            rumqtt::client::Notification::Publish(ref message) => {
+                let topic = &message.topic_name;
+                let data = &message.payload.as_slice();
+
+                // Processing of backend messages
+                if topic.contains(&format!("apps/{}", config.backend)) {
+                    match janus::handle_message(&mut tx, data) {
+                        Err(err) => handle_error(topic, data, err),
+                        Ok(_) => info!("Message has been processed"),
                     }
-                    _ => error!("bad envelope format = {:?}", message),
+                } else {
+                    error!("Received a message with an unexpected topic = {}", topic)
                 }
             }
-            _ => warn!("unknown message = {:?}", message),
+            _ => error!("An unsupported type of message = {:?}", message),
         }
     }
 }
 
-pub(crate) fn mqtt_options(client_id: &str, config: &config::Mqtt) -> Result<MqttOptions, Error> {
-    let uri = config.uri.parse::<http::Uri>()?;
-    let host = uri.host().ok_or_else(|| err_msg("missing MQTT host"))?;
-    let port = uri
-        .port_part()
-        .ok_or_else(|| err_msg("missing MQTT port"))?;
-
-    Ok(MqttOptions::new(client_id, host, port.as_u16()).set_keep_alive(30))
+fn handle_error(topic: &str, data: &[u8], error: Error) {
+    let message = std::str::from_utf8(data).unwrap_or("[non-utf8 characters]");
+    warn!(
+        "Processing of a message = {} from a topic = {} failed because of an error = {}.",
+        message, topic, error
+    );
 }
--- src/backend/janus.rs
+++ src/backend/janus.rs
@@ -1,22 +1,39 @@
-use crate::transport;
-use failure::{err_msg, Error};
 use serde_derive::{Deserialize, Serialize};
-use std::fmt;
-use std::str::FromStr;
 
 ////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Debug, Serialize)]
-pub(crate) struct SessionCreateRequest {
-    transaction: Transaction,
+pub(crate) struct CreateSessionRequest {
+    transaction: String,
     janus: String,
 }
 
-impl SessionCreateRequest {
-    pub(crate) fn new(transaction: Transaction) -> Self {
+impl CreateSessionRequest {
+    pub(crate) fn new(transaction: &str) -> Self {
         Self {
-            transaction: transaction,
+            transaction: transaction.to_owned(),
             janus: "create".to_owned(),
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug, Serialize)]
+pub(crate) struct CreateHandleRequest {
+    transaction: String,
+    session_id: u64,
+    plugin: String,
+    janus: String,
+}
+
+impl CreateHandleRequest {
+    pub(crate) fn new(transaction: &str, session_id: u64, plugin: &str) -> Self {
+        Self {
+            transaction: transaction.to_owned(),
+            session_id,
+            plugin: plugin.to_owned(),
+            janus: "attach".to_owned(),
         }
     }
 }
@@ -27,97 +44,55 @@ impl SessionCreateRequest {
 #[serde(rename_all = "lowercase")]
 #[serde(tag = "janus")]
 pub(crate) enum Response {
+    Error(ErrorResponse),
     Success(SuccessResponse),
-    Timeout(SessionTimeoutEvent),
+    Timeout(TimeoutEvent),
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub(crate) enum ErrorResponse {
+    Handle(HandleErrorResponse),
+    Session(SessionErrorResponse),
+}
+
+#[derive(Debug, Deserialize)]
+pub(crate) struct SessionErrorResponse {
+    pub(crate) transaction: String,
+    pub(crate) error: ErrorResponseData,
+}
+
+#[derive(Debug, Deserialize)]
+pub(crate) struct HandleErrorResponse {
+    pub(crate) transaction: String,
+    pub(crate) session_id: u64,
+    pub(crate) error: ErrorResponseData,
+}
+
+#[derive(Debug, Deserialize)]
+pub(crate) struct ErrorResponseData {
+    pub(crate) code: u32,
+    pub(crate) reason: String,
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Debug, Deserialize)]
 pub(crate) struct SuccessResponse {
-    transaction: Transaction,
-    data: SessionResponseData,
+    pub(crate) transaction: String,
+    pub(crate) data: SuccessResponseData,
 }
 
 #[derive(Debug, Deserialize)]
-pub(crate) struct SessionResponseData {
-    id: u64,
+pub(crate) struct SuccessResponseData {
+    pub(crate) id: u64,
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Debug, Deserialize)]
-pub(crate) struct SessionTimeoutEvent {
-    session_id: u64,
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-#[derive(Debug)]
-pub(crate) struct Transaction {
-    id: String,
-    agent_id: transport::AgentId,
-}
-
-impl Transaction {
-    pub(crate) fn new(id: &str, agent_id: transport::AgentId) -> Self {
-        Self {
-            id: id.to_owned(),
-            agent_id: agent_id,
-        }
-    }
-}
-
-impl fmt::Display for Transaction {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}.{}", self.id, self.agent_id)
-    }
-}
-
-impl FromStr for Transaction {
-    type Err = Error;
-
-    fn from_str(val: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<&str> = val.splitn(2, '.').collect();
-        match parts[..] {
-            [id, rest] => Ok(Self::new(&id, rest.parse::<transport::AgentId>()?)),
-            _ => Err(err_msg(format!("Invalid value for transaction: {}", val))),
-        }
-    }
-}
-
-impl serde::Serialize for Transaction {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        serializer.serialize_str(&self.to_string())
-    }
-}
-
-struct TransactionVisitor;
-
-impl<'de> serde::de::Visitor<'de> for TransactionVisitor {
-    type Value = Transaction;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(formatter, "a FQDN compatible value")
-    }
-
-    fn visit_str<E>(self, val: &str) -> Result<Self::Value, E>
-    where
-        E: serde::de::Error,
-    {
-        val.parse::<Transaction>()
-            .map_err(|_| E::invalid_value(serde::de::Unexpected::Str(val), &self))
-    }
-}
-
-impl<'de> serde::Deserialize<'de> for Transaction {
-    fn deserialize<D>(deserializer: D) -> Result<Transaction, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        deserializer.deserialize_str(TransactionVisitor)
-    }
+pub(crate) struct TimeoutEvent {
+    pub(crate) session_id: u64,
 }
