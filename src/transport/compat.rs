09e3241ee6c77ede3c794dd564c71fef297392aa
Andrei Nesterov <ae.nesterov@gmail.com>
2018-12-17 11:17:23 -0800

    Distinguish transport::compat::LocalEnvelope

--- src/transport/compat.rs
+++ src/transport/compat.rs
@@ -2,14 +2,14 @@ use super::MessageProperties;
 use failure::Error;
 use serde_derive::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
-pub(crate) struct Envelope {
+#[derive(Debug, Serialize)]
+pub(crate) struct LocalEnvelope {
     payload: String,
     #[serde(skip_serializing_if = "Option::is_none")]
     properties: Option<MessageProperties>,
 }
 
-impl Envelope {
+impl LocalEnvelope {
     pub(crate) fn new(payload: &str, properties: Option<MessageProperties>) -> Self {
         Self {
             payload: payload.to_owned(),
@@ -21,22 +21,38 @@ impl Envelope {
 pub(crate) fn to_envelope<T>(
     payload: &T,
     properties: Option<MessageProperties>,
-) -> Result<Envelope, Error>
+) -> Result<LocalEnvelope, Error>
 where
     T: serde::Serialize,
 {
     let payload_s = serde_json::to_string(payload)?;
-    let envelope = Envelope::new(&payload_s, properties);
+    let envelope = LocalEnvelope::new(&payload_s, properties);
     Ok(envelope)
+}
+
+#[derive(Debug, Deserialize)]
+pub(crate) struct Envelope {
+    payload: String,
+    properties: MessageProperties,
+}
+
+impl Envelope {
+    pub(crate) fn properties(&self) -> &MessageProperties {
+        &self.properties
+    }
+
+    pub(crate) fn payload<T>(&self) -> Result<T, Error>
+    where
+        T: serde::de::DeserializeOwned,
+    {
+        let payload = serde_json::from_str::<T>(&self.payload)?;
+        Ok(payload)
+    }
 }
 
 pub(crate) fn from_envelope<T>(envelope: Envelope) -> Result<(T, MessageProperties), Error>
 where
     T: serde::de::DeserializeOwned,
 {
-    let payload = serde_json::from_str::<T>(&envelope.payload)?;
-    let properties = envelope
-        .properties
-        .expect("An incoming message envelope must contain properties.");
-    Ok((payload, properties))
+    Ok((envelope.payload::<T>()?, envelope.properties))
 }
