5e8d40a36ee1ee0a9bdc4a38346357ee3c50d98c
Andrei Nesterov <ae.nesterov@gmail.com>
2018-12-21 18:43:39 -0800

    Create a demo session

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -52,7 +52,47 @@ pub(crate) struct AuthnMessageProperties {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize, FromSqlRow, AsExpression)]
+#[sql_type = "sql::Account_id"]
+pub struct AccountId {
+    label: String,
+    audience: String,
+}
+
+impl AccountId {
+    pub(crate) fn new(label: &str, audience: &str) -> Self {
+        Self {
+            label: label.to_owned(),
+            audience: audience.to_owned(),
+        }
+    }
+}
+
+impl fmt::Display for AccountId {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}.{}", self.label, self.audience)
+    }
+}
+
+impl FromStr for AccountId {
+    type Err = Error;
+
+    fn from_str(val: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = val.splitn(2, '.').collect();
+        match parts[..] {
+            [ref label, ref audience] => Ok(Self::new(label, audience)),
+            _ => Err(err_msg(format!(
+                "Invalid value for the application name: {}",
+                val
+            ))),
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug, Clone, Deserialize, Serialize, FromSqlRow, AsExpression)]
+#[sql_type = "sql::Agent_id"]
 pub(crate) struct AgentId {
     label: String,
     account_id: AccountId,
@@ -115,44 +155,6 @@ impl From<&AuthnMessageProperties> for AgentId {
 ////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
-pub(crate) struct AccountId {
-    label: String,
-    audience: String,
-}
-
-impl AccountId {
-    pub(crate) fn new(label: &str, audience: &str) -> Self {
-        Self {
-            label: label.to_owned(),
-            audience: audience.to_owned(),
-        }
-    }
-}
-
-impl fmt::Display for AccountId {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}.{}", self.label, self.audience)
-    }
-}
-
-impl FromStr for AccountId {
-    type Err = Error;
-
-    fn from_str(val: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<&str> = val.splitn(2, '.').collect();
-        match parts[..] {
-            [ref label, ref audience] => Ok(Self::new(label, audience)),
-            _ => Err(err_msg(format!(
-                "Invalid value for the application name: {}",
-                val
-            ))),
-        }
-    }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-#[derive(Debug, Clone, Deserialize, Serialize)]
 pub(crate) struct SharedGroup {
     label: String,
     account_id: AccountId,
@@ -192,3 +194,64 @@ impl FromStr for SharedGroup {
 
 pub(crate) mod compat;
 pub(crate) mod correlation_data;
+
+pub mod sql {
+
+    use super::AccountId;
+    use super::AgentId;
+
+    use diesel::deserialize::{self, FromSql};
+    use diesel::pg::Pg;
+    use diesel::serialize::{self, Output, ToSql, WriteTuple};
+    use diesel::sql_types::{Record, Text};
+    use std::io::Write;
+
+    #[derive(SqlType)]
+    #[postgres(type_name = "account_id")]
+    #[allow(non_camel_case_types)]
+    pub struct Account_id;
+
+    impl ToSql<Account_id, Pg> for AccountId {
+        fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
+            WriteTuple::<(Text, Text)>::write_tuple(&(&self.label, &self.audience), out)
+        }
+    }
+
+    impl FromSql<Account_id, Pg> for AccountId {
+        fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
+            let (label, audience): (String, String) =
+                FromSql::<Record<(Text, Text)>, Pg>::from_sql(bytes)?;
+            Ok(AccountId::new(&label, &audience))
+        }
+    }
+
+    #[derive(SqlType)]
+    #[postgres(type_name = "agent_id")]
+    #[allow(non_camel_case_types)]
+    pub struct Agent_id;
+
+    impl ToSql<Agent_id, Pg> for AgentId {
+        fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
+            WriteTuple::<(Text, Text, Text)>::write_tuple(
+                &(
+                    &self.account_id.label,
+                    &self.account_id.audience,
+                    &self.label,
+                ),
+                out,
+            )
+        }
+    }
+
+    impl FromSql<Agent_id, Pg> for AgentId {
+        fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
+            let (agent_label, account_label, audience): (String, String, String) =
+                FromSql::<Record<(Text, Text, Text)>, Pg>::from_sql(bytes)?;
+            Ok(AgentId::new(
+                &agent_label,
+                AccountId::new(&account_label, &audience),
+            ))
+        }
+    }
+
+}
