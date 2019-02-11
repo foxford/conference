9784cba0e70e7ab6756ba7b69aa18addaada00d6
Andrei Nesterov <ae.nesterov@gmail.com>
2019-02-10 20:19:54 -0800

    Move authn and authz to crates.io

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -1,4 +1,3 @@
-use crate::authn::Authenticable;
 use failure::{format_err, Error};
 use serde_derive::{Deserialize, Serialize};
 use std::fmt;
@@ -8,62 +7,6 @@ use std::str::FromStr;
 
 pub(crate) trait Addressable: Authenticable {
     fn agent_label(&self) -> &str;
-}
-////////////////////////////////////////////////////////////////////////////////
-
-#[derive(Debug, Clone, Deserialize, Serialize, FromSqlRow, AsExpression)]
-#[sql_type = "sql::Account_id"]
-pub(crate) struct AccountId {
-    label: String,
-    audience: String,
-}
-
-impl AccountId {
-    pub(crate) fn new(label: &str, audience: &str) -> Self {
-        Self {
-            label: label.to_owned(),
-            audience: audience.to_owned(),
-        }
-    }
-
-    pub(crate) fn label(&self) -> &str {
-        &self.label
-    }
-
-    pub(crate) fn audience(&self) -> &str {
-        &self.audience
-    }
-}
-
-impl fmt::Display for AccountId {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}.{}", self.label, self.audience)
-    }
-}
-
-impl FromStr for AccountId {
-    type Err = Error;
-
-    fn from_str(val: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<&str> = val.splitn(2, '.').collect();
-        match parts[..] {
-            [ref label, ref audience] => Ok(Self::new(label, audience)),
-            _ => Err(format_err!(
-                "invalid value for the application name: {}",
-                val
-            )),
-        }
-    }
-}
-
-impl Authenticable for AccountId {
-    fn account_label(&self) -> &str {
-        &self.label
-    }
-
-    fn audience(&self) -> &str {
-        &self.audience
-    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -93,11 +36,13 @@ impl AgentId {
 }
 
 impl fmt::Display for AgentId {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         write!(
-            f,
+            fmt,
             "{}.{}.{}",
-            self.label, self.account_id.label, self.account_id.audience,
+            self.label(),
+            self.account_id.label(),
+            self.account_id.audience(),
         )
     }
 }
@@ -120,11 +65,11 @@ impl FromStr for AgentId {
 
 impl Authenticable for AgentId {
     fn account_label(&self) -> &str {
-        &self.account_id.label
+        &self.account_id.label()
     }
 
     fn audience(&self) -> &str {
-        &self.account_id.audience
+        &self.account_id.audience()
     }
 }
 
@@ -204,33 +149,14 @@ pub(crate) enum Source<'a> {
 
 pub mod sql {
 
-    use super::AccountId;
     use super::AgentId;
+    use authn::AccountId;
 
     use diesel::deserialize::{self, FromSql};
     use diesel::pg::Pg;
     use diesel::serialize::{self, Output, ToSql, WriteTuple};
     use diesel::sql_types::{Record, Text};
     use std::io::Write;
-
-    #[derive(SqlType, QueryId)]
-    #[postgres(type_name = "account_id")]
-    #[allow(non_camel_case_types)]
-    pub struct Account_id;
-
-    impl ToSql<Account_id, Pg> for AccountId {
-        fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
-            WriteTuple::<(Text, Text)>::write_tuple(&(&self.label, &self.audience), out)
-        }
-    }
-
-    impl FromSql<Account_id, Pg> for AccountId {
-        fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
-            let (label, audience): (String, String) =
-                FromSql::<Record<(Text, Text)>, Pg>::from_sql(bytes)?;
-            Ok(AccountId::new(&label, &audience))
-        }
-    }
 
     #[derive(SqlType, QueryId)]
     #[postgres(type_name = "agent_id")]
@@ -251,9 +177,12 @@ pub mod sql {
         }
     }
 
+    pub use authn::sql::Account_id;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+
+pub(crate) use authn::{AccountId, Authenticable};
 
 pub(crate) mod correlation_data;
 pub(crate) mod mqtt;
