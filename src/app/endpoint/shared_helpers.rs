052f55629f2d5e1fb4ceb5d6ea0c1c18d1be3d64
Timofey Martynov <feymartynov@users.noreply.github.com>
2019-09-30 16:17:58 +0300

    Check agent presence in the room on leave (#64)

---
+++ src/app/endpoint/shared_helpers.rs
@@ -0,0 +1,31 @@
+use diesel::pg::PgConnection;
+use svc_agent::{mqtt::ResponseStatus, AgentId};
+use svc_error::Error as SvcError;
+
+use crate::db::{agent, room};
+
+pub(crate) fn check_room_presence(
+    room: &room::Object,
+    agent_id: &AgentId,
+    conn: &PgConnection,
+) -> Result<(), SvcError> {
+    let results = agent::ListQuery::new()
+        .room_id(room.id())
+        .agent_id(agent_id)
+        .execute(conn)?;
+
+    if results.len() == 0 {
+        let err = SvcError::builder()
+            .status(ResponseStatus::NOT_FOUND)
+            .detail(&format!(
+                "agent = '{}' is not online in the room = '{}'",
+                agent_id,
+                room.id()
+            ))
+            .build();
+
+        Err(err)
+    } else {
+        Ok(())
+    }
+}
