f3f6b6f4409d26004c30427c093bd739add94d13
Andrei Nesterov <ae.nesterov@gmail.com>
2020-03-11 21:14:37 +0300

    Allow to specify a number of minimum idle database connections

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -5,10 +5,11 @@ use std::time::Duration;
 
 pub(crate) type ConnectionPool = Arc<Pool<ConnectionManager<PgConnection>>>;
 
-pub(crate) fn create_pool(url: &str, size: u32, timeout: u64) -> ConnectionPool {
+pub(crate) fn create_pool(url: &str, size: u32, idle_size: Option<u32>, timeout: u64) -> ConnectionPool {
     let manager = ConnectionManager::<PgConnection>::new(url);
     let pool = Pool::builder()
         .max_size(size)
+        .min_idle(idle_size)
         .connection_timeout(Duration::from_secs(timeout))
         .build(manager)
         .expect("Error creating a database pool");
