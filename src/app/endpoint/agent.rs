25a37b3af16e8d6c3ddf2cebc8acb6018fcdcaf5
Timofey Martynov <feymartynov@gmail.com>
2020-05-15 17:20:21 +0300

    Optimize DB connection usage

--- src/app/endpoint/agent.rs
+++ src/app/endpoint/agent.rs
@@ -33,15 +33,17 @@ impl RequestHandler for ListHandler {
         reqp: &IncomingRequestProperties,
         start_timestamp: DateTime<Utc>,
     ) -> Result {
-        let conn = context.db().get()?;
-
         // Check whether the room exists and open.
-        let room = db::room::FindQuery::new()
-            .id(payload.room_id)
-            .time(db::room::now())
-            .execute(&conn)?
-            .ok_or_else(|| format!("the room = '{}' is not found or closed", payload.room_id))
-            .status(ResponseStatus::NOT_FOUND)?;
+        let room = {
+            let conn = context.db().get()?;
+
+            db::room::FindQuery::new()
+                .id(payload.room_id)
+                .time(db::room::now())
+                .execute(&conn)?
+                .ok_or_else(|| format!("the room = '{}' is not found or closed", payload.room_id))
+                .status(ResponseStatus::NOT_FOUND)?
+        };
 
         // Authorize agents listing in the room.
         let room_id = room.id().to_string();
@@ -53,15 +55,19 @@ impl RequestHandler for ListHandler {
             .await?;
 
         // Get agents list in the room.
-        let agents = db::agent::ListQuery::new()
-            .room_id(payload.room_id)
-            .status(db::agent::Status::Ready)
-            .offset(payload.offset.unwrap_or_else(|| 0))
-            .limit(std::cmp::min(
-                payload.limit.unwrap_or_else(|| MAX_LIMIT),
-                MAX_LIMIT,
-            ))
-            .execute(&conn)?;
+        let agents = {
+            let conn = context.db().get()?;
+
+            db::agent::ListQuery::new()
+                .room_id(payload.room_id)
+                .status(db::agent::Status::Ready)
+                .offset(payload.offset.unwrap_or_else(|| 0))
+                .limit(std::cmp::min(
+                    payload.limit.unwrap_or_else(|| MAX_LIMIT),
+                    MAX_LIMIT,
+                ))
+                .execute(&conn)?
+        };
 
         // Respond with agents list.
         Ok(Box::new(stream::once(shared::build_response(
