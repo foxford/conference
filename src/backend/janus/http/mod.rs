9840cee950b78db7368756591105efe4cf3271fd
Ilya Titkov <ilya@titkov.me>
2021-06-08 15:37:13 +0300

    Add correct body

--- src/backend/janus/http/mod.rs
+++ src/backend/janus/http/mod.rs
@@ -1,9 +1,10 @@
 use std::sync::Arc;
 
 use isahc::{http::Uri, AsyncReadResponseExt, HttpClient, Request};
+use uuid::Uuid;
 
 use self::create_handle::{CreateHandleRequest, CreateHandleResponse};
-
+use serde_json::json;
 pub mod create_handle;
 
 #[derive(Debug)]
@@ -24,8 +25,14 @@ impl JanusClient {
         &self,
         request: &CreateHandleRequest,
     ) -> anyhow::Result<CreateHandleResponse> {
+        let body = serde_json::to_vec(&json!( {
+            "janus": "attach",
+            "transaction": Uuid::new_v4().to_string(),
+            "session_id": request.session_id,
+            "plugin": "janus.plugin.conference",
+        }))?;
         let request =
-            Request::post(format!("{}/{}", self.janus_url, request.session_id)).body(())?;
+            Request::post(format!("{}/{}", self.janus_url, request.session_id)).body(body)?;
         Ok(self.http.send_async(request).await?.json().await?)
     }
 }
