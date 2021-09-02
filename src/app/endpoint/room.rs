efa6218522011630ed3c98ac58c7d7a9c8bbb626
Shamir Khodzha <khodzha.sh@gmail.com>
2021-09-02 17:28:19 +0300

    logging verbosity around room update handler

--- src/app/endpoint/room.rs
+++ src/app/endpoint/room.rs
@@ -24,6 +24,7 @@ use crate::{
     db,
     db::{room::RoomBackend, rtc::SharingPolicy as RtcSharingPolicy},
 };
+use tracing::warn;
 use tracing_attributes::instrument;
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -211,6 +212,8 @@ impl RequestHandler for UpdateHandler {
         } else {
             helpers::RoomTimeRequirement::Any
         };
+        warn!(?time_requirement, "Room update");
+
         let conn = context.get_conn().await?;
 
         let room = task::spawn_blocking({
@@ -218,6 +221,8 @@ impl RequestHandler for UpdateHandler {
             move || helpers::find_room_by_id(id, time_requirement, &conn)
         })
         .await?;
+
+        warn!(id = ?room.id(), "Room update, room found");
 
         // Authorize room updating on the tenant.
         let room_id = room.id().to_string();
@@ -230,6 +235,7 @@ impl RequestHandler for UpdateHandler {
         context.metrics().observe_auth(authz_time);
 
         let room_was_open = !room.is_closed();
+        warn!(room_was_open, "Room update, room_was_open");
 
         // Update room.
         let conn = context.get_conn().await?;
@@ -271,6 +277,8 @@ impl RequestHandler for UpdateHandler {
                 }
             };
 
+            warn!(old_time = ?room.time(), new_time = ?time, "Room update, time update");
+
             Ok::<_, AppError>(db::room::UpdateQuery::new(room.id())
                 .time(time)
                 .reserve(payload.reserve)
@@ -278,6 +286,10 @@ impl RequestHandler for UpdateHandler {
                 .classroom_id(payload.classroom_id)
                 .execute(&conn)?)
         }).await?;
+
+        warn!(
+            new_time = ?room.time(), "Room update, room updated"
+        );
 
         // Respond and broadcast to the audience topic.
         let response = helpers::build_response(
@@ -300,7 +312,11 @@ impl RequestHandler for UpdateHandler {
 
         // Publish room closed notification.
         if let (_, Bound::Excluded(closed_at)) = room.time() {
+            warn!("Room update, room closed at is present");
+
             if room_was_open && *closed_at <= Utc::now() {
+                warn!("Room update, room closed at is in the past");
+
                 let room = async_std::task::spawn_blocking({
                     let room_id = room.id();
                     let agent = reqp.as_agent_id().to_owned();
@@ -308,6 +324,9 @@ impl RequestHandler for UpdateHandler {
                     move || db::room::set_closed_by(room_id, &agent, &conn)
                 })
                 .await?;
+
+                warn!(closed_by = ?reqp.as_agent_id(), "Room update, room closed_by is set");
+
                 responses.push(helpers::build_notification(
                     "room.close",
                     &format!("rooms/{}/events", room.id()),
@@ -325,6 +344,9 @@ impl RequestHandler for UpdateHandler {
                 ));
             }
         }
+
+        warn!("Room update, done, responses len = {:?}", responses.len());
+
         context
             .metrics()
             .request_duration
