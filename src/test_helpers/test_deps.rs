f56147b82c8dbfae79bb581b90b9412d14b35e2c
Ilya Titkov <ilya@titkov.me>
2021-10-18 16:50:12 +0300

    Update janus

--- src/test_helpers/test_deps.rs
+++ src/test_helpers/test_deps.rs
@@ -36,7 +36,7 @@ impl LocalDeps {
 
     pub fn run_janus(&self) -> JanusHandle {
         let base_dir = env!("CARGO_MANIFEST_DIR");
-        let image = images::generic::GenericImage::new("foxford/janus-gateway", "0cae20e")
+        let image = images::generic::GenericImage::new("foxford/janus-gateway", "4aee743")
             .with_volume(
                 format!("{}/{}", base_dir, "src/test_helpers/janus_confs/"),
                 "/opt/janus/etc/janus/",
