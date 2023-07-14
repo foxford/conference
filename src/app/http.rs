87e735fc31f5e6ce966ff409d2c242314c5c6993
wakeful-kitten <andor_@bk.ru>
2023-07-14 17:17:42 +0300

    Switch to metrics-middleware from svc-utils

--- src/app/http.rs
+++ src/app/http.rs
@@ -13,6 +13,7 @@ use futures::future::BoxFuture;
 use http::{Method, Request};
 use hyper::{body::HttpBody, Body};
 use svc_agent::mqtt::{Agent, IntoPublishableMessage};
+use svc_utils::middleware::MeteredRoute;
 use tower::{layer::layer_fn, Service};
 use tower_http::trace::TraceLayer;
 use tracing::{
@@ -21,11 +22,7 @@ use tracing::{
     info, Span,
 };
 
-use super::{
-    context::{AppContext, GlobalContext},
-    endpoint,
-    error::ErrorKind,
-};
+use super::{context::AppContext, endpoint};
 use crate::app::message_handler::publish_message;
 
 pub fn build_router(
@@ -34,49 +31,48 @@ pub fn build_router(
     authn: svc_authn::jose::ConfigMap,
 ) -> Router {
     let router = Router::new()
-        .route("/rooms/:id/agents", get(endpoint::agent::list))
-        .route(
+        .metered_route("/rooms/:id/agents", get(endpoint::agent::list))
+        .metered_route(
             "/rooms/:id/configs/reader",
             get(endpoint::agent_reader_config::read).post(endpoint::agent_reader_config::update),
         )
-        .route(
+        .metered_route(
             "/rooms/:id/configs/writer",
             get(endpoint::agent_writer_config::read).post(endpoint::agent_writer_config::update),
         )
-        .route("/rooms/:id/enter", post(endpoint::room::enter))
-        .route("/rooms/:id/close", post(endpoint::room::close))
-        .route("/rooms", post(endpoint::room::create))
-        .route(
+        .metered_route("/rooms/:id/enter", post(endpoint::room::enter))
+        .metered_route("/rooms/:id/close", post(endpoint::room::close))
+        .metered_route("/rooms", post(endpoint::room::create))
+        .metered_route(
             "/rooms/:id",
             get(endpoint::room::read).patch(endpoint::room::update),
         )
-        .route(
+        .metered_route(
             "/rooms/:id/rtcs",
             get(endpoint::rtc::list).post(endpoint::rtc::create),
         )
-        .route(
+        .metered_route(
             "/rooms/:id/groups",
             get(endpoint::group::list).post(endpoint::group::update),
         )
-        .route("/rtcs/:id", get(endpoint::rtc::read))
-        .route("/rtcs/:id/streams", post(endpoint::rtc::connect))
-        .route("/rooms/:id/streams", get(endpoint::rtc_stream::list))
-        .route("/streams/signal", post(endpoint::rtc_signal::create))
-        .route("/rtcs/:id/signal", post(endpoint::rtc::connect_and_signal))
-        .route("/streams/trickle", post(endpoint::rtc_signal::trickle))
-        .route(
+        .metered_route("/rtcs/:id", get(endpoint::rtc::read))
+        .metered_route("/rtcs/:id/streams", post(endpoint::rtc::connect))
+        .metered_route("/rooms/:id/streams", get(endpoint::rtc_stream::list))
+        .metered_route("/streams/signal", post(endpoint::rtc_signal::create))
+        .metered_route("/rtcs/:id/signal", post(endpoint::rtc::connect_and_signal))
+        .metered_route("/streams/trickle", post(endpoint::rtc_signal::trickle))
+        .metered_route(
             "/rooms/:id/configs/writer/snapshot",
             get(endpoint::writer_config_snapshot::read),
         )
         .layer(layer_fn(|inner| NotificationsMiddleware { inner }))
-        .layer(layer_fn(|inner| MetricsMiddleware { inner }))
         .layer(Extension(context))
         .layer(Extension(agent))
         .layer(Extension(Arc::new(authn)))
         .layer(svc_utils::middleware::CorsLayer);
     let router = Router::new().nest("/api/v1", router);
 
-    let pingz_router = Router::new().route(
+    let pingz_router = Router::new().metered_route(
         "/healthz",
         get(|| async { Response::builder().body(Body::from("pong")).unwrap() }),
     );
@@ -179,47 +175,6 @@ where
                 for notification in notifications {
                     publish_message(&mut agent, notification)
                 }
-            }
-
-            Ok(res)
-        })
-    }
-}
-
-#[derive(Clone)]
-struct MetricsMiddleware<S> {
-    inner: S,
-}
-
-impl<S, ReqBody, ResBody> Service<Request<ReqBody>> for MetricsMiddleware<S>
-where
-    S: Service<Request<ReqBody>, Response = Response<ResBody>> + Clone + Send + 'static,
-    S::Future: Send + 'static,
-    ReqBody: Send + 'static,
-    ResBody: Send + 'static,
-{
-    type Response = S::Response;
-    type Error = S::Error;
-    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
-
-    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.inner.poll_ready(cx)
-    }
-
-    fn call(&mut self, req: Request<ReqBody>) -> Self::Future {
-        // best practice is to clone the inner service like this
-        // see https://github.com/tower-rs/tower/issues/547 for details
-        let clone = self.inner.clone();
-        let mut inner = std::mem::replace(&mut self.inner, clone);
-
-        Box::pin(async move {
-            let ctx = req.extensions().get::<Arc<AppContext>>().cloned().unwrap();
-            let res: Response<ResBody> = inner.call(req).await?;
-
-            if res.status().is_success() {
-                ctx.metrics().observe_app_ok();
-            } else if let Some(error_kind) = res.extensions().get::<ErrorKind>() {
-                ctx.metrics().observe_app_error(error_kind);
             }
 
             Ok(res)
